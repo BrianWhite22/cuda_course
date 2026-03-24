0d5b62f6eab1f804fc55248c5330ab71bc776e7f
BrianWhite22 <66137533+BrianWhite22@users.noreply.github.com>
2026-03-24 16:58:33 -0400

    Implement error handling for CUDA operations (I hope this doesnt actually send a change proposal, I am not really sure how GitHub works)
    Added error handling for CUDA calls to improve debugging.

--- 01-add-error-handling.cu
+++ 01-add-error-handling.cu
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <cuda_runtime.h> //Might be fine w/o 
+
+#define CHECK(call) \
+do { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+      fprintf(stderr, "cuda error at %s:%d: %s\n", \
+        __FILE__ , __LINE__, cudaGetErrorString(err)); \
+      return 1; \
+    } \
+} while (0)
 
 void init(int *a, int N)
 {
@@ -16,7 +27,7 @@ void doubleElements(int *a, int N)
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = gridDim.x * blockDim.x;
 
-  for (int i = idx; i < N + stride; i += stride)
+  for (int i = idx; i < N + stride; i += stride)  //This is an error
   {
     a[i] *= 2;
   }
@@ -32,9 +43,11 @@ bool checkElementsAreDoubled(int *a, int N)
   return true;
 }
 
+
 int main()
 {
-  /*
+  
+/*
    * Add error handling to this source code to learn what errors
    * exist, and then correct them. Googling error messages may be
    * of service if actions for resolving them are not clear to you.
@@ -44,7 +57,7 @@ int main()
   int *a;
 
   size_t size = N * sizeof(int);
-  cudaMallocManaged(&a, size);
+  CHECK(cudaMallocManaged(&a, size));
 
   init(a, N);
 
@@ -52,10 +65,11 @@ int main()
   size_t number_of_blocks = 32;
 
   doubleElements<<<number_of_blocks, threads_per_block>>>(a, N);
-  cudaDeviceSynchronize();
+  CHECK(cudaGetLastError());
+  CHECK(cudaDeviceSynchronize());
 
   bool areDoubled = checkElementsAreDoubled(a, N);
   printf("All elements were doubled? %s\n", areDoubled ? "TRUE" : "FALSE");
 
-  cudaFree(a);
+  CHECK(cudaFree(a));
 }
